b4ceab955040f13a0fdef5b52124ddfb37ff9ada
shivam-kapila <shivamkapila4@gmail.com>
2018-06-26 19:38:31 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/shivamkapila/myprofile/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.shivamkapila.myprofile
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.homescreen.*
+
+class MainActivity : AppCompatActivity() {
+
+    var photo: ImageView? = null
+    var eduButton: Button? = null
+    var workButton: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homescreen)
+
+        photo = findViewById(R.id.profilePhoto)
+
+        photo?.setOnClickListener({
+            var clickIntent = Intent(this@MainActivity,ProfilePhoto::class.java)
+       startActivity(clickIntent) })
+
+       eduButton = findViewById(R.id.education)
+
+        eduButton?.setOnClickListener({
+            var clickIntent = Intent(this@MainActivity,Education::class.java)
+            startActivity(clickIntent) })
+
+        workButton = findViewById(R.id.workExperience)
+
+        workButton?.setOnClickListener({
+            var clickIntent = Intent(this@MainActivity,WorkExperience::class.java)
+            startActivity(clickIntent) })
+    }
+}
